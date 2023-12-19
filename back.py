94884f57b94dcec9c6d0bc646c2cf2c355ddbc81
GostNOON <onno.burmeister@gmx.de>
2023-12-19 19:47:01 +0100

    def

--- back.py
+++ back.py
@@ -1,15 +1,17 @@
 spieler=3
 einsaetz_spiele=[]
 ergebniss=0
-for i in range(spieler+1):
-    einsaetz_spiele.append(0)
 
-while True:
+def einsatz():
+    for i in range(spieler+1):
+        einsaetz_spiele.append(0)
+
     for i in range(1,spieler+1,1):
         x=int(input(f"einsatz spieler {i}: "))
         einsaetz_spiele[i]=x
 
-    break    
-for i in einsaetz_spiele:
-    ergebniss+=i
+def rechnung_gewinn():
+    for i in einsaetz_spiele:
+        ergebniss+=i
+
 print(ergebniss)
