d47e41007f711582193464fd28e9d459fa88db30
GostNOON <onno.burmeister@gmx.de>
2023-12-19 17:15:37 +0100

    spielr und einsatz

---
+++ back.py
@@ -0,0 +1,5 @@
+spieler=0
+einsaetz_spiele=[]
+for i in range(spieler):
+    einsaetz_spiele.append(0)
+
--- main1.py
+++ main1.py
@@ -2,6 +2,7 @@ import pygame
 #from pygame.locals import *
 from image import *
 from random import *
+from back import *
 
 pygame.init()
 
