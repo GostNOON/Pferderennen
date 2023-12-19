fbffb1d8263bb378f2bb45b78ff4099ceda87c9a
GostNOON <onno.burmeister@gmx.de>
2023-12-19 19:44:24 +0100

    spieler anzahl

--- back.py
+++ back.py
@@ -1,11 +1,11 @@
-spieler=0
+spieler=3
 einsaetz_spiele=[]
 ergebniss=0
 for i in range(spieler+1):
     einsaetz_spiele.append(0)
 
 while True:
-    for i in range(spieler+1):
+    for i in range(1,spieler+1,1):
         x=int(input(f"einsatz spieler {i}: "))
         einsaetz_spiele[i]=x
 
