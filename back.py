afd19925d3059794d1a83373f3e1b613219afea9
GostNOON <onno.burmeister@gmx.de>
2023-12-19 17:23:49 +0100

    einsatz plazierbar

--- back.py
+++ back.py
@@ -1,5 +1,15 @@
 spieler=0
 einsaetz_spiele=[]
-for i in range(spieler):
+ergebniss=0
+for i in range(spieler+1):
     einsaetz_spiele.append(0)
 
+while True:
+    for i in range(spieler+1):
+        x=int(input(f"einsatz spieler {i}: "))
+        einsaetz_spiele[i]=x
+
+    break    
+for i in einsaetz_spiele:
+    ergebniss+=i
+print(ergebniss)
